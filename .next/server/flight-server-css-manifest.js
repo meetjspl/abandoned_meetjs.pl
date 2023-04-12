e9a004b892d26223a93611e9f01282e9915e1bc2
Kamil Sobik <kamil.sobik@gmail.com>
2023-04-12 19:36:49 +0200

    first version of main page

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,15 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/app/page.tsx": [
+      "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/app/layout.tsx": [
+      "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/app/page": [
+      "/Users/kamilsobik/Desktop/Project/meetjs/meetjs.pl/app/globals.css"
+    ]
+  }
+}
