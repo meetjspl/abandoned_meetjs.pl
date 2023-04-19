308f48ca1c2d303033dec839f201225c7bd1b7d6
Michał Miszczyszyn <michal@mmiszy.pl>
2023-04-19 14:48:28 +0200

    Add ESLint config and GitHub Actions (#11)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const _inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
