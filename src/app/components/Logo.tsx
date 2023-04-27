c59ec0d189cbd6baf7bbe4704194fdf509a89f77
Jakub Hajduk <jakubhajduk1@outlook.com>
2023-04-27 19:24:32 +0200

    add-social-media-buttons

---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Logo() {
+	return (
+		<>
+			<Image width={1280} height={400} src="../meetjs-logo.svg" alt={''} />
+		</>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import { FaFacebook, FaDiscord } from 'react-icons/fa'
+import { CgWebsite } from 'react-icons/cg'
+import Link from 'next/link'
+import Logo from './components/Logo'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello meetjs.pl!</h1>
-    </main>
-  );
+	return (
+		<>
+			<main className="flex min-h-screen flex-col items-center p-10 ">
+				<Logo />
+				<Link href="https://facebook.com/meetjspl/">
+					<button className=" text-2xl rounded-full bg-blue-600 px-9 py-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
+						<FaFacebook className="mr-2 inline mb-1  " />
+						Facebook
+					</button>
+				</Link>
+				<Link href="https://discord.gg/UycCSpRh6j">
+					<button className=" text-2xl rounded-full bg-blue-600 px-9 p-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
+						<FaDiscord className="mr-2 inline mb-1 " />
+						Discord
+					</button>
+				</Link>
+				<Link href="https://summit.meetjs.pl/2023/">
+					<button className=" text-2xl rounded-full bg-blue-600 px-9 p-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
+						<CgWebsite className="mr-2 inline mb-1 " />
+						Summit
+					</button>
+				</Link>
+			</main>
+		</>
+	)
 }
