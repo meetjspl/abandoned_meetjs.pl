04b76147033ae176ee11285dc74e756e1f03b93f
Jakub Hajduk <jakubhajduk1@outlook.com>
2023-05-18 22:10:05 +0200

    Button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,26 @@
+import { type ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+const defaultStyles = " cursor-pointer m-10 w-60 px-9  py-5 text-2xl ";
+const shapes = {
+	square: "",
+	pill: "rounded-full ",
+};
+const variants = {
+	default: "primary",
+	social: twMerge(defaultStyles, "primary"),
+};
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	shape?: keyof typeof shapes;
+	variant?: keyof typeof variants;
+};
+
+export default function Button({
+	className,
+	variant = "default",
+	shape = "square",
+	...props
+}: ButtonProps) {
+	return <button className={twMerge(shapes[shape], variants[variant], className)} {...props} />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-import { FaFacebook, FaDiscord } from 'react-icons/fa'
-import { CgWebsite } from 'react-icons/cg'
-import Link from 'next/link'
-import Logo from './components/Logo'
-const inter = Inter({ subsets: ['latin'] })
+import { FaFacebook, FaDiscord } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
+import Link from "next/link";
+import Logo from "./components/Logo";
+import Button from "./components/Button";
+const _inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
@@ -12,24 +13,24 @@ export default function Home() {
 			<main className="flex min-h-screen flex-col items-center p-10 ">
 				<Logo />
 				<Link href="https://facebook.com/meetjspl/">
-					<button className=" text-2xl rounded-full bg-blue-600 px-9 py-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
-						<FaFacebook className="mr-2 inline mb-1  " />
+					<Button shape="pill" variant="social" className=" duration-300 ease-in hover:scale-110">
+						<FaFacebook className="mb-1 mr-2 inline  " />
 						Facebook
-					</button>
+					</Button>
 				</Link>
 				<Link href="https://discord.gg/UycCSpRh6j">
-					<button className=" text-2xl rounded-full bg-blue-600 px-9 p-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
-						<FaDiscord className="mr-2 inline mb-1 " />
+					<Button shape="pill" variant="social" className=" duration-300 ease-in hover:scale-110">
+						<FaDiscord className="mb-1 mr-2 inline " />
 						Discord
-					</button>
+					</Button>
 				</Link>
 				<Link href="https://summit.meetjs.pl/2023/">
-					<button className=" text-2xl rounded-full bg-blue-600 px-9 p-5 text-white cursor-pointer hover:scale-110 ease-in duration-300 button-blue w-60 m-10">
-						<CgWebsite className="mr-2 inline mb-1 " />
+					<Button shape="pill" variant="social" className=" duration-300 ease-in hover:scale-110">
+						<CgWebsite className="mb-1 mr-2 inline " />
 						Summit
-					</button>
+					</Button>
 				</Link>
 			</main>
 		</>
-	)
+	);
 }
