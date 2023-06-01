9cdb4a5102eb90028e5f2ce11de9b48ba4d48a73
Jakub Hajduk <jakubhajduk1@outlook.com>
2023-06-01 15:23:20 +0200

    /src/app/**/* in tailwind.config.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
-		"./src/**/*.{js,ts,jsx,tsx}",
+		"./src/app/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
