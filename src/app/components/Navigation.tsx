ab64326455024579ee27f7077cecba0843655063
Jakub Hajduk <jakubhajduk1@outlook.com>
2023-08-19 18:40:31 +0200

    some more work on navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,48 +1,83 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { type HtmlHTMLAttributes } from "react";
-import ExternalLink from "./ExternalLink";
+import { useState, type HtmlHTMLAttributes } from "react";
 
+import { twMerge } from "tailwind-merge";
+import { useIsAboveBreakpoint } from "../hooks/useIsAboveBreakpoint";
+import ExternalLink from "./ExternalLink";
+import Socials from "./Socials";
 type NavLink = {
 	text: string;
 	href: string;
 	external?: true;
 };
 type NavProps = { links?: NavLink[] } & HtmlHTMLAttributes<HTMLDivElement>;
+const itemStyles = "ease h-0.5 w-6 bg-white transition duration-300";
 export default function Navigation({ links, ...props }: NavProps) {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const isAboveBreakpoint = useIsAboveBreakpoint({ breakpoint: 640 });
+	const isOpen = isMobileMenuOpen && !isAboveBreakpoint;
+	const toggleMenu = () => {
+		setIsMobileMenuOpen(!isOpen);
+		console.log(isOpen ? "open" : "closed");
+	};
+
 	return (
 		<>
 			<nav
 				{...props}
-				className=" fixed z-20 flex w-full flex-row-reverse  justify-around bg-purple opacity-90 "
-				style={{ height: 68 }}
-			>
-				{links?.map((link) =>
-					link.external ? (
-						<ExternalLink
-							key={link.text}
-							className="flex flex-wrap   content-center text-white "
-							href={link.href}
-						>
-							{link.text}
-						</ExternalLink>
-					) : (
-						<Link
-							key={link.text}
-							className="flex flex-wrap   content-center text-white "
-							href={link.href}
-						>
-							{link.text}
-						</Link>
-					),
+				className={twMerge(
+					"fixed z-20 flex h-16 w-full flex-row overflow-y-auto bg-purple sm:relative sm:flex sm:flex-col sm:gap-5 sm:overflow-y-visible sm:py-0 sm:text-sm",
+					isOpen ? "h-full" : "h-16",
 				)}
-				<Image
-					className="justify-center pl-4  pt-4"
-					width={100}
-					height={30}
-					src="../logo-no-border.svg"
-					alt={""}
-				/>
+			>
+				<div className="absolute mr-0 pl-4  pt-4">
+					<Image width={100} height={30} src="../logo-no-border.svg" alt={""} />
+				</div>
+				<button
+					onClick={toggleMenu}
+					className={twMerge(
+						" right-4 z-40 my-4 flex h-8 w-8 flex-col items-center justify-center gap-1.5 sm:hidden",
+						isOpen ? "fixed" : "absolute",
+					)}
+				>
+					<div className={twMerge(itemStyles, isOpen && "translate-y-2 rotate-45")} />
+					<div className={twMerge(itemStyles, isOpen ? "opacity-0" : "opacity-100")} />
+					<div className={twMerge(itemStyles, isOpen && "-translate-y-2 -rotate-45")} />
+				</button>
+				<div
+					className={twMerge(
+						"mb-auto flex  w-full flex-col items-center justify-center gap-10 self-center px-7 sm:flex-row sm:justify-end sm:gap-5",
+					)}
+				>
+					<ul
+						className={twMerge(
+							"bg-primary fixed inset-0 z-30 flex flex-col items-center gap-5 overflow-y-auto py-3 text-xl text-white max-sm:py-20 sm:relative sm:my-1 sm:flex  sm:flex-row sm:items-center sm:justify-around  sm:overflow-y-visible ",
+							isOpen ? "flex" : "hidden",
+						)}
+					>
+						{links?.map((link) =>
+							link.external ? (
+								<li key={link.text}>
+									<ExternalLink className="sm:px-7" href={link.href}>
+										{link.text}
+									</ExternalLink>
+								</li>
+							) : (
+								<li key={link.text}>
+									<Link className="sm:px-7" key={link.text} href={link.href}>
+										{link.text}
+									</Link>
+								</li>
+							),
+						)}
+					</ul>
+					<Socials
+						className={twMerge("s relative top-96 sm:static sm:flex", isOpen ? "block" : "hidden")}
+					/>
+				</div>
 			</nav>
 		</>
 	);
--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -1,21 +1,22 @@
 import { BsTwitter, BsLinkedin, BsFacebook, BsDiscord } from "react-icons/bs";
+import { twMerge } from "tailwind-merge";
 import ExternalLink from "./ExternalLink";
-
-export default function Socials() {
+type props = { className?: string };
+export default function Socials({ className }: props) {
 	return (
 		<>
-			<div className="flex flex-row content-center px-7 py-5">
+			<div className={twMerge("flex flex-row content-center ", className)}>
 				<ExternalLink href={""}>
-					<BsTwitter className="mx-2 text-xl text-white"></BsTwitter>
+					<BsTwitter className="mx-2 my-2 text-xl text-white"></BsTwitter>
 				</ExternalLink>
 				<ExternalLink href={""}>
-					<BsLinkedin className="mx-2 text-xl text-white"></BsLinkedin>
+					<BsLinkedin className="mx-2 my-2 text-xl text-white"></BsLinkedin>
 				</ExternalLink>
 				<ExternalLink href={""}>
-					<BsFacebook className="mx-2 text-xl text-white"></BsFacebook>
+					<BsFacebook className="mx-2 my-2 text-xl text-white"></BsFacebook>
 				</ExternalLink>
 				<ExternalLink href={""}>
-					<BsDiscord className="mx-2 text-xl text-white"></BsDiscord>
+					<BsDiscord className="mx-2 my-2 text-xl text-white"></BsDiscord>
 				</ExternalLink>
 			</div>
 		</>
